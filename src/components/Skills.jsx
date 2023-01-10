37552367816596f144f4ca9de4d01b117f42770c
Ilona Ma <ilona.ma@aalto.fi>
2023-01-10 17:13:04 +0200

    Badges for skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,35 +1,78 @@
-import { Col, Container, Row, ProgressBar, Tabs, Tab } from "react-bootstrap";
+import { Col, Container, Row, Tabs, Tab, Badge } from "react-bootstrap";
 import { Progress } from "reactstrap";
+import StarRating from "react-bootstrap-star-rating";
+import { CircularProgressbar } from "react-circular-progressbar";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "react-circular-progressbar/dist/styles.css";
 
 const Skills = () => {
   return (
-    <div className="skills-container-bg">
+    <div className="skills-container-bg" id="skills">
       <Container className="skills-container">
         <Row>
           <h2>Skills</h2>;
         </Row>
         <Row>
-          <Tabs
-            defaultActiveKey="programming"
-            className="tab-bar-pill"
-            fill
-            justify
-          >
-            <Tab eventKey="programming" title="Programming" className="tabs">
-              <Container>
-                <p>Scala</p>
-                <Progress barClassName="skill-bar" value={90} />
-                <p>React native</p>
-                <Progress barClassName="skill-bar" value={70} />
-                <p>React</p>
-                <Progress barClassName="skill-bar" value={60} />
-              </Container>
+          <Tabs defaultActiveKey="technical" className="tab-bar-pill" fill>
+            <Tab eventKey="technical" title="Technical" className="tabs">
+              <Technical />
             </Tab>
-            <Tab eventKey="languages" title="Languages" className="tabs"></Tab>
+            <Tab eventKey="languages" title="Languages" className="tabs">
+              <Technical />
+            </Tab>
           </Tabs>
         </Row>
       </Container>
     </div>
+  );
+};
+
+const Technical = () => {
+  return (
+    <Container>
+      <div>
+        <h5>
+          <Badge className="badge" bg="secondary">
+            Scala
+          </Badge>
+        </h5>
+        <Progress barClassName="skill-bar" value={90} />
+      </div>
+      <div>
+        <h5>
+          <Badge className="badge" bg="secondary">
+            HTML/CSS
+          </Badge>
+          <Badge className="badge" bg="secondary">
+            Javascript
+          </Badge>
+        </h5>
+        <Progress barClassName="skill-bar" value={75} />
+      </div>
+      <div>
+        <h5>
+          <Badge className="badge" bg="secondary">
+            Git
+          </Badge>
+          <Badge className="badge" bg="secondary">
+            Azure DevOps
+          </Badge>
+        </h5>
+        <Progress barClassName="skill-bar" value={50} />
+      </div>
+      <div>
+        <h5>
+          <Badge className="badge" bg="secondary">
+            Python
+          </Badge>
+          <Badge className="badge" bg="secondary">
+            C/C++
+          </Badge>
+        </h5>{" "}
+        <Progress barClassName="skill-bar" value={40} />
+      </div>
+    </Container>
   );
 };
 
