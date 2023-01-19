27267f6521c02c52011cbe39f83c80faa7b62870
Ilona Ma <ilona.ma@aalto.fi>
2023-01-19 14:11:49 +0200

    Changed footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,62 +1,85 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
-import { AiFillMail, AiOutlineMail, AiTwotonePhone } from "react-icons/ai";
+import React from "react";
+import { MDBFooter, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { useTranslation } from "react-i18next";
+import { Nav } from "react-bootstrap";
 
 const Footer = () => {
+  const { t } = useTranslation("translation", { keyPrefix: "footer" });
   return (
-    <div className="footer py-4 px-2 bg-dark">
-      <Container>
-        <Row className="d-flex flex-rowmx-auto align-items-start mx-auto">
-          <Col className="border-end">
-            <h4 className="fw-bold text-uppercase">Image credits</h4>
-            <ul>
-              <li>
-                Banner image by{" "}
-                <a
-                  href="https://www.freepik.com/free-vector/gradient-galaxy-background_14658088.htm#query=space&position=3&from_view=search&track=sph"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Freepik
-                </a>
-              </li>
-              <li>
-                Night sky stars background photo by{" "}
-                <a
-                  href="https://unsplash.com/@mumbolicious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Samuel Richard
-                </a>{" "}
-                on{" "}
-                <a
-                  href="https://unsplash.com/photos/UWY8nGROvbk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Unsplash
-                </a>
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <h4 className="fw-bold text-uppercase">Contact</h4>
-            <ul className="contact-list">
-              <li className="">
-                <AiFillMail className="mx-2" />
-                <a variant="link" href="mailto:ilona.ma0909@gmail.com">
-                  Email me
-                </a>
-              </li>
-              <li>
-                <AiTwotonePhone className="mx-2" />
-                <a href="tel:040 4165492">040 4165492</a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <MDBFooter className="" color="white" bgColor="dark">
+      <MDBContainer className="p-4">
+        <section className="">
+          <MDBRow>
+            <MDBCol className="mb-4 mb-md-0">
+              <h5 className="text-uppercase fw-bold">{t("credits.title")}</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li className="mb-3">
+                  {t("credits.banner")}{" "}
+                  <a href="https://www.freepik.com/free-vector/gradient-galaxy-background_14658088.htm#query=space&position=3&from_view=search&track=sph">
+                    Freepik
+                  </a>
+                </li>
+                <li>
+                  {t("credits.background")}{" "}
+                  <a
+                    href="https://unsplash.com/@mumbolicious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Samuel Richard
+                  </a>{" "}
+                  {"("}
+                  <a
+                    href="https://unsplash.com/photos/UWY8nGROvbk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Unsplash
+                  </a>
+                  {")"}
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol className="mb-4 mb-md-0">
+              <h5 className="text-uppercase fw-bold">{t("contact.title")}</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li className="">{t("contact.mail")}</li>
+                <li className="d-flex flex-row justify-content-start">
+                  {" "}
+                  <Nav.Link
+                    href="https://github.com/ilonanmaa"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fs-1 me-2"
+                  >
+                    <AiFillGithub />
+                  </Nav.Link>
+                  <Nav.Link
+                    href="https://www.linkedin.com/in/ilonama/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fs-1 ms-2"
+                  >
+                    <AiFillLinkedin />
+                  </Nav.Link>
+                </li>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center p-3 copyright-bar"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2023 Copyright: Ilona Ma
+      </div>
+    </MDBFooter>
   );
 };
 
